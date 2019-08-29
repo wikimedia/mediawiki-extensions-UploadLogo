7271b751c33ffa0e1222e9c6477391f99ab40d3d
Fomafix <fomafix@googlemail.com>
2019-08-29 15:37:11 +0200

    Update JavaScript code according to coding conventions
    Change-Id: I8f0cbcbe26bd306662b6afc3b6050b53cd2e634c

--- modules/ext.upload_logo.js
+++ modules/ext.upload_logo.js
@@ -1,31 +1,31 @@
-$(document).ready(function(){
-	$('.check-image').click(function () {
-		$('#selected-logo').val($(this).val()); 
-	});
-	
-	if ($('#logo-file' ).val()){
-	//	$('#logo-file' ).attr('id').attr( 'checked', true );
-	//	$('#logo-file' ).attr('id').parent().css( 'background-color', '#fee' );
+$( function () {
+	$( '.check-image' ).on( 'click', function () {
+		$( '#selected-logo' ).val( $( this ).val() );
+	} );
+
+	if ( $( '#logo-file' ).val() ) {
+	//	$( '#logo-file' ).attr( 'id' ).attr( 'checked', true );
+	//	$( '#logo-file' ).attr( 'id' ).parent().css( 'background-color', '#fee' );
 	}
-	
-	var conf = mw.config.get(['wgServer','wgScript']);
-	
-	$('#change-logo' ).click(function () { 
-		var img=$('.check-image:checked').val();
-		$.post(conf.wgServer + conf.wgScript,{'title':'Special:Upload_Logo','selected':encodeURIComponent(img)},function(data){
-			if(data.result=='success') {
-				$('#p-logo a').css('background-image','none').css('background-image', 'url( /images/logo/'+data.logofile+')' );
+
+	var conf = mw.config.get( [ 'wgServer', 'wgScript' ] );
+
+	$( '#change-logo' ).on( 'click', function () {
+		var img = $( '.check-image:checked' ).val();
+		$.post( conf.wgServer + conf.wgScript, { title: 'Special:Upload_Logo', selected: encodeURIComponent( img ) }, function ( data ) {
+			if ( data.result === 'success' ) {
+				$( '#p-logo a' ).css( 'background-image', 'none' ).css( 'background-image', 'url( /images/logo/' + data.logofile + ')' );
 			}
-		},"json");
-	});
-	
-	$('#delete-logo' ).click(function () {
-		var img=$('.check-image:checked').val();
-		$.post(conf.wgServer + conf.wgScript,{'title':'Special:Upload_Logo','delete':encodeURIComponent(img)},function(data){
-			if(data.result=='success') {
-				$('.check-image:checked').parent().remove();
-			}							
-		},"json");
+		}, 'json' );
+	} );
+
+	$( '#delete-logo' ).on( 'click', function () {
+		var img = $( '.check-image:checked' ).val();
+		$.post( conf.wgServer + conf.wgScript, { title: 'Special:Upload_Logo', 'delete': encodeURIComponent( img ) }, function ( data ) {
+			if ( data.result === 'success' ) {
+				$( '.check-image:checked' ).parent().remove();
+			}
+		}, 'json' );
 		/*
 		var request = new Json.Remote( wgServer + wgScript + '?title=Special:Upload_Logo&delete=' + encodeURIComponent( selectedFile ), {
 			method: 'get',
@@ -34,10 +34,10 @@ $(document).ready(function(){
 					$$( '.check-image' ).each( function ( another2 ) {
 						another2.setProperty( 'disabled', false );
 						another2.getParent().setStyle( 'background-color', '#fff' );
-					});
+					} );
 					$( selectedFile ).getParent().remove();
 				}
 			}
-		}).send(); */
-	});
-});
+		} ).send(); */
+	} );
+} );
